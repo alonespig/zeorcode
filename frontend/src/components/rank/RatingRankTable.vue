a368cc74bd4245fa2b68807bf84640a8a4ddb827
chenyk <libai2644@gmail.com>
2026-09-21 01:27:24 +0800

    style(rank): 统一 rating 榜数字样式

--- frontend/src/components/rank/RatingRankTable.vue
+++ frontend/src/components/rank/RatingRankTable.vue
@@ -2,14 +2,16 @@
   <table class="data-table">
     <colgroup>
       <col style="width: 10%;" />
-      <col style="width: 50%;" />
       <col style="width: 20%;" />
-      <col style="width: 20%;" />
+      <col style="width: 40%;" />
+      <col style="width: 15%;" />
+      <col style="width: 15%;" />
     </colgroup>
     <thead>
       <tr>
         <th>排名</th>
         <th class="left">用户名</th>
+        <th>个性签名</th>
         <th>Rating</th>
         <th>最高分</th>
       </tr>
@@ -25,8 +27,9 @@
             <UserName :name="u.username" :rating="u.rating" clickable @click="$emit('click-user', u.id)" />
           </div>
         </td>
-        <td class="font-bold tabular-nums" :style="{ color: tierColor(u.rating) }">{{ u.rating }}</td>
-        <td class="tabular-nums text-gray-400">{{ u.maxRating || u.rating }}</td>
+        <td>{{ u.signature }}</td>
+        <td> {{ u.rating }} </td>
+        <td> {{ u.maxRating || u.rating }} </td>
       </tr>
     </tbody>
   </table>
@@ -34,14 +37,12 @@
 
 <script setup>
 import UserName from '@/components/UserName.vue'
-import { ratingTier } from '@/constants/index'
 
 defineProps({
   list: { type: Array, default: () => [] },
 })
 defineEmits(['click-user'])
 
-const tierColor = (r) => ratingTier(r).color
 const medalClass = (index) =>
   index === 1 ? 'font-bold text-[#e6a817]'
     : index === 2 ? 'font-bold text-[#9aa7b4]'
